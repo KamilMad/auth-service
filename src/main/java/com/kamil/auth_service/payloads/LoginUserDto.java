7d49d94fea5d2265d295ad696b683e207cab14e4
KamilMad <kamilul085@gmail.com>
2025-02-05 22:12:07 +0100

    Implement authenticate method

--- src/main/java/com/kamil/auth_service/payloads/LoginUserDto.java
+++ src/main/java/com/kamil/auth_service/payloads/LoginUserDto.java
@@ -1,5 +1,10 @@
 package com.kamil.auth_service.payloads;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginUserDto {
     private String email;
     private String password;
--- src/main/java/com/kamil/auth_service/services/AuthenticationService.java
+++ src/main/java/com/kamil/auth_service/services/AuthenticationService.java
@@ -1,9 +1,11 @@
 package com.kamil.auth_service.services;
 
 import com.kamil.auth_service.model.User;
+import com.kamil.auth_service.payloads.LoginUserDto;
 import com.kamil.auth_service.payloads.RegisterUserDto;
 import com.kamil.auth_service.repository.UserRepository;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -27,5 +29,14 @@ public class AuthenticationService {
         return userRepository.save(user);
     }
 
+    public User authenticate(LoginUserDto input) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getEmail(),
+                        input.getPassword()
+                        )
+        );
 
+        return userRepository.findByEmail(input.getEmail()).orElseThrow();
+    }
 }
